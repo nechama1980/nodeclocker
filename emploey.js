ac6a5586b8953a8cc36a0baff312f1a2fd9bbe47
nechama1980 <nf0548461980@gmail.com>
2020-07-12 18:55:31 +0300

    first commit

---
+++ emploey.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const mainRouter = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+
+mainRouter.get('/GetEmploeyDetails_phone_and_name', function (req, res) {
+  console.log("hi")
+  var emploeys = null;
+  // Connection url
+  const url = 'mongodb://localhost:27017';
+  // Database Name
+  const dbName = 'clocker';
+  // Connect using MongoClient
+  MongoClient.connect(url, function (err, client) {
+    // Create a collection we want to drop later
+    const col = client.db(dbName).collection('emploey');
+    // Insert a bunch of documents
+
+    col.find({},{projection: { _id:0,firstName: 1, phone: 1 }}).toArray(function (err, result) {
+      console.log(err);
+      console.log(result);
+      if (err) {
+        res.send('<b>error</b>');
+      } else {
+        res.send(result);
+      }
+    })
+  });
+});
+mainRouter.get('/GetFullDetails/:id', function (req, res) {
+  console.log(req.params.id)
+  var emploeys = null;
+  // Connection url
+  const url = 'mongodb://localhost:27017';
+  // Database Name
+  const dbName = 'clocker';
+  // Connect using MongoClient
+  MongoClient.connect(url, function (err, client) {
+    // Create a collection we want to drop later
+    const col = client.db(dbName).collection('emploey');
+    // Insert a bunch of documents
+    col.find({tz:req.params.id}).toArray(function (err, result) {
+      if (err) {
+        res.send('<b>error</b>');
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    })
+  });
+});
+
+mainRouter.get('/deleteEmp/:id', function(req, res) {
+  console.log(req.params.id)
+  var emploeys = null;
+  // Connection url
+  const url = 'mongodb://localhost:27017';
+  // Database Name
+  const dbName = 'clocker';
+  // Connect using MongoClient
+  MongoClient.connect(url, function (err, client) {
+    // Create a collection we want to drop later
+    const col = client.db(dbName).collection('emploey');
+    // Insert a bunch of documents
+    col.updateOne(({tz:req.params.id},{$set:{isactive:"false"}}),function(err, result){
+      if (err) {
+        res.send('<b>error</b>');
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    })
+  });
+});
+
+
+mainRouter.post('/addEmploey',function (req, res) {
+  console.log(req.body)
+  var emploeys = null;
+  // Connection url
+  const url = 'mongodb://localhost:27017';
+  // Database Name
+  const dbName = 'clocker';
+  // Connect using MongoClient
+  MongoClient.connect(url, function (err, client) {
+    // Create a collection we want to drop later
+    const col = client.db(dbName).collection('emploey');
+    // Insert a bunch of documents
+    col.insert(req.body, function (err, result) {
+      console.log(err);
+      console.log(result);
+      if (err) {
+        res.send('<b>error</b>');
+      } else {
+        res.send('<b>sucess</b>');
+      }
+    })
+  });
+});
+
+module.exports = mainRouter;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const emploey = require('./emploey');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var server = app.listen(3000, function () {
+    console.log("listen to port 3000")
+})
+
+//פונקציה שתמיד מגיע אליה קודם ואז ממשיך הלאה כי יש לה נקסט
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+})
+
+app.get('/', function (req, res) {
+    res.send('<b>Hello World!</b>');
+});
+app.use('/emploey', emploey);
+//בסוף אחרי שהוא עבר אחד אחד ולא יצא לו להכנס לאף אחד... כי לא מצא כזה ניתוב...
+app.use(function (req, res, next) {
+    res.send("url not found");
+});
+
+
+
+
+
