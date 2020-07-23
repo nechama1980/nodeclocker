b2d2ee0cd97b4ad097f1ef624d4fb543fedd39e5
nechama1980 <nf0548461980@gmail.com>
2020-07-23 21:38:22 +0300

    update project

--- emploey.js
+++ emploey.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mainRouter = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 
+//פרטי כל העובדים : טלפון שם פרטי ומשפחה
 mainRouter.get('/GetEmploeyDetails_phone_and_name', function (req, res) {
   console.log("hi")
   var emploeys = null;
@@ -10,12 +11,14 @@ mainRouter.get('/GetEmploeyDetails_phone_and_name', function (req, res) {
   // Database Name
   const dbName = 'clocker';
   // Connect using MongoClient
-  MongoClient.connect(url, function (err, client) {
+  MongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }, function (err, client) {
     // Create a collection we want to drop later
     const col = client.db(dbName).collection('emploey');
     // Insert a bunch of documents
-
-    col.find({},{projection: { _id:0,firstName: 1,lastName: 1, phone: 1 }}).toArray(function (err, result) {
+    col.find({}, { projection: { _id: 0, firstName: 1, lastName: 1, phone: 1 } }).toArray(function (err, result) {
       console.log(err);
       console.log(result);
       if (err) {
@@ -27,7 +30,7 @@ mainRouter.get('/GetEmploeyDetails_phone_and_name', function (req, res) {
     })
   });
 });
-
+//פרטים של עובד יחיד לפי ת.ז
 mainRouter.get('/GetFullDetails/:id', function (req, res) {
   console.log(req.params.id)
   var emploeys = null;
@@ -40,7 +43,7 @@ mainRouter.get('/GetFullDetails/:id', function (req, res) {
     // Create a collection we want to drop later
     const col = client.db(dbName).collection('emploey');
     // Insert a bunch of documents
-    col.findOne({ tz: req.params.id },function (err, result) {
+    col.findOne({ tz: req.params.id }, function (err, result) {
       if (err) {
         res.send('<b>error</b>');
       } else {
@@ -50,22 +53,40 @@ mainRouter.get('/GetFullDetails/:id', function (req, res) {
     })
   });
 });
-
-
-
-mainRouter.get('/deleteEmp/:id', function(req, res) {
-  console.log(req.params.id)
-  var emploeys = null;
-  // Connection url
+//עדכון לכולם
+mainRouter.put('/UPDATEALL', function (req, res) {
   const url = 'mongodb://localhost:27017';
   // Database Name
   const dbName = 'clocker';
   // Connect using MongoClient
   MongoClient.connect(url, function (err, client) {
     const col = client.db(dbName).collection('emploey');
-    var myquery = { tz: req.params.id };
-    var newvalues = { $set: { firstName: "new name" } };
-    col.updateOne(newvalues,myquery,function(err, result){
+    var newvalues = { $set: { isactive: false } };
+    col.update(newvalues, function (err, result) {
+      if (err) {
+        throw err;
+        res.send('<b>error</b>');
+      } else {
+        console.log(result);
+        res.send(result);
+      }
+    })
+  });
+});
+//מחיקת עובד
+mainRouter.post('/deleteEmp', function(req, res) {
+  console.log(req.body.id);
+  var emploeys = null;
+  // Connection url
+  const url = 'mongodb://localhost:27017';
+  // Database Name
+  const dbName = 'clocker';
+  // Connect using MongoClient
+  MongoClient.connect(url,{ useUnifiedTopology: true }, function (err, client) {
+    const col = client.db(dbName).collection('emploey');
+    var myquery = { tz: req.body.id };
+    var newvalues = { $set: { "isactive": "false"} };
+    col.findOneAndUpdate(myquery,newvalues,function(err, result){
       if (err) {
         res.send('<b>error</b>');
       } else {
@@ -75,14 +96,7 @@ mainRouter.get('/deleteEmp/:id', function(req, res) {
     })
   });
 });
-
-
-
-
-
-
-
-
+//הוספת עובד
 mainRouter.post('/addEmploey', function (req, res) {
   console.log(req.body)
   var emploeys = null;
@@ -106,5 +120,4 @@ mainRouter.post('/addEmploey', function (req, res) {
     })
   });
 });
-
 module.exports = mainRouter;
